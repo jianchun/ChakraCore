caa045d659661f76f46eb43a9288f6fe9d0f6e59
Jianchun Xu <jianxu@microsoft.com>
2016-02-10 17:30:50 -0800

    works for lib/common

--- case.js
+++ case.js
@@ -1,13 +1,51 @@
 // Known words to be converted to CamelCase
 var knownWords = [
-    'lib', 'common', 'codex', 'chakra', 'utf8'
+    'lib', 'common', 'codex', 'chakra', 'utf8', 'defines', 'min',
+    'target', 'ver', 'math', 'util',
+    'int16', 'int32', 'int64',
+    'rejit', 'reason', 'get', 'current', 'frame', 'event',
+    'daylight', 'time', 'helper', 'logger',
+    'number', 'utilities', 'jobs', 'thread', 'service', 'tick', 'date',
+    'base', 'byte', 'swap', 'smart', 'control', 'enum', 'back', 'end',
+    'data', 'structures', 'doubly', 'linked', 'list',
+    'fixed', 'bit', 'vector', 'line', 'offset', 'cache',
+    'dictionary', 'internal', 'string', 'no', 'case', 'comparer',
+    'continuous', 'page', 'stack', 'eval', 'map', 'simple', 'hash', 'table',
+    'large', 'character', 'buffer', 'sparse', 'array', 'size', 'policy',
+    'unit', 'big', 'int', 'immutable', 'regex', 'key', 'growing',
+    'value', 'pair', 'mru', 'leaf', 'interval', 'weak', 'reference',
+    'template', 'parameter', 'exceptions', 'exception', 'asm', 'js', 'parse',
+    'operation', 'aborted', 'report', 'error', 'out', 'of', 'memory', 'overflow',
+    'not', 'implemented', 'disabled', 'script', 'abort',
+    'recycler', 'heuristic', 'small', 'normal', 'heap', 'block', 'root', 'ptr',
+    'info', 'allocation', 'fast', 'allocator', 'custom', 'finalizable',
+    'forced', 'constraints', 'virtual', 'alloc', 'wrapper',
+    'write', 'barrier', 'manager', 'stress', 'test', 'object', 'graph', 'dumper',
+    'leak', 'bucket', 'free', 'valid', 'pointers', 'gen', 'build',
+    'arena', 'mark', 'watson', 'telemetry', 'idle', 'decommit', 'collection', 'state',
+    'auto', 'type', 'filter', 'etw', 'tracking', 'place', 'holder',
+    'external', 'method', 'core', 'config', 'flags', 'profile', 'cmd',
+    'perf', 'counter', 'fault', 'injection', 'dbg', 'help', 'symbol', 'trace',
+    'atom', 'lock', 'guids', 'sys', 'binary', 'feature', 'critical', 'section',
+    'delay', 'load', 'resource', 'class', 'pinned', 'default', 'pch', 'reasons',
+    'element', 'option', 'queue', 'stats', 'entry', 'builder', 'enumerator', 'tuple',
+    'tree', 'check', 'throw', 'banned', 'pool', 'sweep', 'operators', 'context',
+    'declarations', 'constants', 'output', 'typedefs', 'max', 'parser', 'instrument',
+    'set', 'assert', 'impl', 'assertions', 'library', 'types', 'warnings', 'basic',
+
+];
+
+// Known words to be in upper case
+var knownUpperCaseWords = [
+    'id', 'cfg', 'fpu', 'i', 'x'
 ];
 
 // Reserved words to remain unchanged
 var reservedWords = [
-
+    'vtinfo', 'vtregistry', 'vcxproj', 'arm', 'arm64', 'amd64',
+    '_GET_CURRENT_FRAME', 'UInt16', 'UInt32', 'SList', 'DList', 'quicksort', 'vpm',
+    '_SAVE_REGISTERS', 'FILE', 'inl', '_strtod', 'api', 'API', 'Api', '32b', '64b',
 ];
-
 
 function toCamelCase(word) {
     word = word.toLowerCase();
@@ -15,13 +53,6 @@ function toCamelCase(word) {
 }
 
 var dict = new Map();
-
-reservedWords.forEach(w => {
-    if (dict.has(w)) {
-        throw new Error("ERROR: duplicated reservedWords: " + w);
-    }
-    dict.set(w, w);
-});
 
 knownWords.forEach(w => {
     if (dict.has(w)) {
@@ -31,6 +62,27 @@ knownWords.forEach(w => {
     dict.set(w, r);
     dict.set(r, r); // Also put expected in to handle already correct casing.
 });
+knownUpperCaseWords.forEach(w => {
+    if (dict.has(w)) {
+        throw new Error("ERROR: duplicated knownUpperCaseWords: " + w);
+    }
+    var r = w.toUpperCase();
+    dict.set(w, r);
+    dict.set(r, r); // Also put expected in to handle already correct casing.
+});
+reservedWords.forEach(w => {
+    if (dict.has(w)) {
+        throw new Error("ERROR: duplicated reservedWords: " + w);
+    }
+    dict.set(w, w);
+
+    // Assume this list has recommented casing. Try from lowercase -> this.
+    var lower = w.toLowerCase();
+    if (!dict.has(lower)) {
+        dict.set(lower, w);
+    }
+});
+
 
 
 function transformName(name) {
@@ -38,25 +90,34 @@ function transformName(name) {
         return name;
     }
 
+    // if (name == toCamelCase(name)) {
+    //     return name; // ignore if already camel case
+    // }
+
     for (var len = name.length; len > 0; len--) {
-        var w = dict.get(name.substr(0, len));
+        var part = name.substr(0, len);
+        var w = dict.get(part) || dict.get(part.toLowerCase());
         if (w) {
             return w + transformName(name.substr(len));
         }
     }
 
-    throw new Error("ERROR: don't know how to transform: " + name);
+    throw new Error("don't know how to transform: " + name);
 }
 
 module.exports = function(path) {
-    var len = path.lastIndexOf('.');
-    if (len < 0) {
-        len = path.length;
-    }
+    try {
+        var len = path.lastIndexOf('.');
+        if (len < 0) {
+            len = path.length;
+        }
 
-    var bare = path.substr(0, len);
-    var ext = path.substr(len);
-    return bare.split('/').map(name =>
-        name.split('.').map(part => transformName(part)).join('.')
-    ).join('/') + ext;
+        var bare = path.substr(0, len);
+        var ext = path.substr(len);
+        return bare.split('/').map(name =>
+            name.split('.').map(part => transformName(part)).join('.')
+        ).join('/') + ext;
+    } catch(ex) {
+        throw new Error(path + ", " + ex.message);
+    }
 }
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var cli = require('command-line-args')([
         type: Boolean
     },
     {
+        name: 'filenameOnly', alias: 'f',
+        description: '(when test) display filename changes only',
+        type: Boolean, defaultValue: true
+    },
+    {
         name: '?', alias: '?',
         description: 'show usage',
         type: Boolean
@@ -29,14 +34,41 @@ if (options["?"]) {
     process.exit(0);
 }
 
+var fs = require('fs');
+var shell = require('shelljs');
+function rename(oldPath, newPath) {
+    var i = newPath.lastIndexOf('/');
+    if (i >= 0) {
+        var dir = newPath.substr(0, i);
+        shell.mkdir('-p', dir);
+    }
+
+    fs.renameSync(oldPath, newPath);
+}
+
+function filenameOf(path) {
+    var i = path.lastIndexOf('/');
+    return i >= 0 ? path.substr(i + 1) : path;
+}
+
 var transform = require('./case.js');
 process.stdin.pipe(require('split')()).on('data', line => {
+    if (line.length == 0 || !fs.lstatSync(line).isFile()) {
+        return; // skip dirs
+    }
+
     var result = transform(line);
     if (result != line) {
         if (options.test) {
-            console.log(line, "\t", result);
+            if (options.filenameOnly) {
+                line = filenameOf(line);
+                result = filenameOf(result);
+            }
+            if (result != line) {
+                console.log(line, "\t", result);
+            }
         } else {
-            // mv
+            rename(line, result);
         }
     }
 });
